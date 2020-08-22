b94212a7dfa89afbc174104b41efe3bf3d498d41
Trần Văn Trường <ngo2016@gmail.com>
2020-08-22 16:43:33 +0700

    report - done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,19 +1,30 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DoAnThucTap.Models;
+using DoAnThucTap.ViewModel;
 
 namespace DoAnThucTap.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DUAN_KHUCNCEntities db = new DUAN_KHUCNCEntities();
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Report()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            var tables = new RDViewModel
+            {
+                RDCost = db.TY_LE_CHI_RD.ToList(),
+                RDEmployee = db.TY_LE_LAO_DONG_RD.ToList(),
+            };
+            return View(tables);
         }
 
         public ActionResult Contact()
---
+++ ViewModel/RDViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DoAnThucTap.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAnThucTap.ViewModel
+{
+    public class RDViewModel
+    {
+        public IEnumerable<TY_LE_CHI_RD> RDCost { get; set; }
+        public IEnumerable<TY_LE_LAO_DONG_RD> RDEmployee { get; set; }
+    }
+}
