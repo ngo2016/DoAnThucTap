dff7174e4efd75e6c4e520292d0ca9ab66f0af60
Trần Văn Trường <ngo2016@gmail.com>
2020-08-22 22:02:11 +0700

    dropdown created

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,9 +27,41 @@ namespace DoAnThucTap.Controllers
             return View(tables);
         }
 
-        public ActionResult Contact()
+        public ActionResult Prediction()
         {
-            ViewBag.Message = "Your contact page.";
+            List<SelectListItem> mySkills = new List<SelectListItem>() {
+        new SelectListItem {
+            Text = "ASP.NET MVC", Value = "1"
+        },
+        new SelectListItem {
+            Text = "ASP.NET WEB API", Value = "2"
+        },
+        new SelectListItem {
+            Text = "ENTITY FRAMEWORK", Value = "3"
+        },
+        new SelectListItem {
+            Text = "DOCUSIGN", Value = "4"
+        },
+        new SelectListItem {
+            Text = "ORCHARD CMS", Value = "5"
+        },
+        new SelectListItem {
+            Text = "JQUERY", Value = "6"
+        },
+        new SelectListItem {
+            Text = "ZENDESK", Value = "7"
+        },
+        new SelectListItem {
+            Text = "LINQ", Value = "8"
+        },
+        new SelectListItem {
+            Text = "C#", Value = "9"
+        },
+        new SelectListItem {
+            Text = "GOOGLE ANALYTICS", Value = "10"
+        },
+    };
+            ViewBag.MySkills = mySkills;
 
             return View();
         }
