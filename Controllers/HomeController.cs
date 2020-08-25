a88e1408f7423ce01949143279861ccebaa17240
Trần Văn Trường <ngo2016@gmail.com>
2020-08-25 23:47:45 +0700

    fix navbar

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,56 +28,7 @@ namespace DoAnThucTap.Controllers
         }
 
         public ActionResult Prediction()
-        {
-            List<SelectListItem> doanhNghiep = new List<SelectListItem>() {
-                new SelectListItem {
-                    Text = "DaeYoung", Value = "1"
-                },
-                new SelectListItem {
-                    Text = "Daikou", Value = "2"
-                },
-                new SelectListItem {
-                    Text = "Datalogic", Value = "3"
-                },
-                new SelectListItem {
-                    Text = "DGS", Value = "4"
-                },
-                new SelectListItem {
-                    Text = "Jabil", Value = "5"
-                },
-                new SelectListItem {
-                    Text = "Nidec Sankyo ", Value = "6"
-                },
-                new SelectListItem {
-                    Text = "Nidec Servo ", Value = "7"
-                },
-                new SelectListItem {
-                    Text = "Platel Vina", Value = "8"
-                },
-                new SelectListItem {
-                    Text = "Samsung", Value = "9"
-                },
-                new SelectListItem {
-                    Text = "Sanofi", Value = "10"
-                },
-                new SelectListItem {
-                    Text = "Sonion ", Value = "11"
-                },
-                new SelectListItem {
-                    Text = "STID", Value = "12"
-                },
-                new SelectListItem {
-                    Text = "T.C", Value = "13"
-                },
-                new SelectListItem {
-                    Text = "The Gioi GEN", Value = "14"
-                },
-                new SelectListItem {
-                    Text = "United Healthcare ", Value = "15"
-                },
-            };
-            ViewBag.DoanhNghiep = doanhNghiep;
-
+        {            
             return View();
         }
     }
